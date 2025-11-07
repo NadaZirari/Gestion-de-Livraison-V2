b3fab865f9d22a55721fd0bb10a55080f5309f57
Nada Zirari <zirarinada20@gmail.com>
2025-11-07 17:28:13 +0100

    jai ajouter les imports manquants

--- src/main/java/com/delivrey/repository/DeliveryHistoryRepository.java
+++ src/main/java/com/delivrey/repository/DeliveryHistoryRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
