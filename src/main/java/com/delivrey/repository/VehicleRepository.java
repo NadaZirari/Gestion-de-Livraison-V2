08ae156708c67b2bc64eaf14b3e4522de3f86401
Nada Zirari <zirarinada20@gmail.com>
2025-11-10 10:20:50 +0100

    ajouter methode of findbyvolume in vehicule repo

--- src/main/java/com/delivrey/repository/VehicleRepository.java
+++ src/main/java/com/delivrey/repository/VehicleRepository.java
@@ -14,10 +14,13 @@ import org.springframework.data.repository.query.Param;
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
 	
 	 Vehicle findByRegistrationNumber(String registrationNumber);
-	 List <Vehicle>findByMaxVolumeOrderByDesc(double maxVolume);
-	 
-	 @Query("select v from Vehicles v orderby v.maxVolume DESC"  )
-	 List <Vehicle>findByMaxVolume( @Param(value = "maxVolume") double maxVolume);
+    // Find vehicles with maxVolume greater than or equal to the given value, ordered by maxVolume in descending order
+    @Query("SELECT v FROM Vehicle v WHERE v.maxVolume >= :maxVolume ORDER BY v.maxVolume DESC")
+    List<Vehicle> findByMaxVolumeGreaterThanEqualOrderByMaxVolumeDesc(@Param("maxVolume") double maxVolume);
+    
+    // Find vehicles with maxVolume less than or equal to the given value, ordered by maxVolume in ascending order
+    @Query("SELECT v FROM Vehicle v WHERE v.maxVolume <= :maxVolume ORDER BY v.maxVolume ASC")
+    List<Vehicle> findByMaxVolumeLessThanEqualOrderByMaxVolumeAsc(@Param("maxVolume") double maxVolume);
 
 	 
 }
